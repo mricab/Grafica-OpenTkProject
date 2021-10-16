0c56ed2293a0dd03a62ef08263bd4c2dd0b2cc3c
Marcelo Rios <chelo38@gmail.com>
2021-10-16 08:11:34 -0400

    Final House Design

--- OpenTkTutorial/Colors.cs
+++ OpenTkTutorial/Colors.cs
@@ -11,5 +11,8 @@ namespace OpenTkProject
         public static Vector4 Blue = new Vector4(0, 0, 1, 1);
         public static Vector4 Beige = new Vector4(1, 0.9333f, 0.7804f, 1);
         public static Vector4 DarkBeige = new Vector4(0.8627f, 0.8f, 0.651f, 1);
+        public static Vector4 PastelOrange = new Vector4(1f, 0.33f, 0.2039f, 1f);
+        public static Vector4 Concrete = new Vector4(0.75f, 0.75f, 0.75f, 1f);
+        public static Vector4 Wood = new Vector4(0.6f, 0.2862f, 0.1725f, 1f);
     }
 }
--- OpenTkTutorial/Design.cs
+++ OpenTkTutorial/Design.cs
@@ -10,9 +10,17 @@ namespace OpenTkProject
             // Root Object
             RootObject Root = new RootObject();
 
+            // House Foundation
+            Parallelepiped Foundation = new Parallelepiped(0.1f, 1.1f, 2.1f, new Vector3(0.0f, 0.0f, 0.0f), Colors.Concrete);
+            Root.AddObject("Foundation", Foundation);
+
             // House Body
-            Parallelepiped HouseBody = new Parallelepiped(1.0f, 1.0f, 2.0f, new Vector3(0.0f, 0.0f, 0.0f), Colors.DarkBeige);
-            Root.AddObject("House Body", HouseBody);
+            Parallelepiped HouseBody = new Parallelepiped(1.0f, 1.0f, 2.0f, new Vector3(0.0f, 0.5f, 0.0f), Colors.DarkBeige);
+            Foundation.AddObject("House Body", HouseBody);
+
+            // Door
+            Parallelepiped Door = new Parallelepiped(0.5f, 0.3f, 0.025f, new Vector3(0.0f, -0.25f, 1f), Colors.Wood);
+            HouseBody.AddObject("Door", Door);
 
             // House Roof
             Object Roof = new Object(new Vector3(0.0f, 1.0f, 0.0f));
@@ -20,11 +28,11 @@ namespace OpenTkProject
             Roof.AddFace(new Triangle(1f, Colors.DarkBeige, new Vector3(0f, 0f, -1f), new Vector3(0f, 0f, 0f)));
             Roof.AddFace(new Triangle(1f, Colors.DarkBeige, new Vector3(0f, 0f, 1f), new Vector3(0f, 0f, 0f)));
             var aux = 0.5 - MathHelper.Sin(MathHelper.DegreesToRadians(60)) * 0.5;
-            Roof.AddFace(new Rectangle(1f, 2f, Colors.Green, new Vector3(-0.25f, -(float)aux, 0f), new Vector3(0f, -90f, -30f)));
-            Roof.AddFace(new Rectangle(1f, 2f, Colors.Blue, new Vector3(0.25f, -(float)aux, 0f), new Vector3(0f, 90f, 30f)));
+            Roof.AddFace(new Rectangle(1f, 2f, Colors.PastelOrange, new Vector3(-0.25f, -(float)aux, 0f), new Vector3(0f, -90f, -30f)));
+            Roof.AddFace(new Rectangle(1f, 2f, Colors.PastelOrange, new Vector3(0.25f, -(float)aux, 0f), new Vector3(0f, 90f, 30f)));
 
             // House Rotation
-            HouseBody.SetModel(Matrix4.CreateRotationY(MathHelper.DegreesToRadians(-45)));
+            Foundation.SetModel(Matrix4.CreateRotationY(MathHelper.DegreesToRadians(-45)));
 
             // Scene
             Scene Scene = new Scene(Root);
